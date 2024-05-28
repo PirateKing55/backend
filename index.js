699aac7375ca2f980a7385b7a2b6ad9884884838
PirateKing55 <itsamrit26@gmail.com>
2024-05-28 20:02:41 +0530

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 app.use(express.json());
